a7db04324b680d3f9e8e162b8352d90563f66e0a
kabirutd <mohammadkabir@gmail.com>
2019-10-21 22:10:35 -0500

    Add project files.

---
+++ WSApp1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WSApp1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
